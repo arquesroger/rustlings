2454079a1f2a18c4428553c9616156d6c6e1d373
rogerarques <rogerarquesnolla@gmail.com>
2023-09-05 10:34:43 +0200

    exercises

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,24 +5,21 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
-
-    string_uppercase(&data);
+    string_uppercase(&data); // Pass a reference to data
 }
 
-// Should not take ownership
+// Takes ownership and returns the last char
 fn get_char(data: String) -> char {
     data.chars().last().unwrap()
 }
 
-// Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
-
-    println!("{}", data);
+// Borrows the data and converts it to uppercase
+fn string_uppercase(data: &String) {
+    let upper = data.to_uppercase();
+    println!("{}", upper);
 }
